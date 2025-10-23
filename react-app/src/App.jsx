c80bebdde439115d6a43e68fb4aeee73bd4e69c5
nico-gao <jtgao99@gmail.com>
2025-10-23 16:39:47 -0400

    react basic 2

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -3,51 +3,84 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
+const people = [
+  "Creola Katherine Johnson: mathematician",
+  "Mario José Molina-Pasquel Henríquez: chemist",
+  "Percy Lavon Julian: chemist",
+  "Subrahmanyan Chandrasekhar: astrophysicist",
+];
+
 export default function App() {
   const [count, setCount] = useState(0);
 
-  console.log("test");
+  // const listItems = people.map((person) => <li>{person}</li>);
+
+  // console.log("listItems", listItems);
+  // console.log(<Profile />);
+  // console.log(Profile);
   return (
     // jsx -> js, babel (complier)
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      {a}
-      {foo()}
-      <Profile />
+      <ul>
+        {/* {listItems} */}
+        {/* {people.map((person) => (
+          <li>{person}</li>
+        ))} */}
+        {people.map((person, index) => (
+          <Profile name={person} id={index} key={index}></Profile>
+        ))}
+      </ul>
+      <ul>
+        {people.map((person, index) => (
+          <Profile name={person} id={index} key={index}></Profile>
+        ))}
+      </ul>
     </>
   );
 }
 
-function Profile() {
+// props change -> re-render
+function Profile({ name, id, children }) {
+  // console.log(props);
+  console.log(id);
+
+  const onClickHandler = () => {
+    console.log("inside button on click handler");
+  };
+
   return (
     <>
-      <img src={{ src: "123123", link: "qweqweqwe" }} alt="Katherine Johnson" />
-      <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />
+      {children}
+      <img src={"https://i.imgur.com/MK3eW3As.jpg"} alt="Katherine Johnson" />
+      <p>{name}</p>
+      <button onClick={onClickHandler}>this is a button</button>
     </>
   );
 }
+
+// img src
+// App -> Profile -> Avatar
+// unidirection data flow, parent -> child
 
 // export default App;
 
+/**
+ * Layout component
+ *
+ * <div classname="app">
+ *  <nav></nav>
+ *  {children}
+ * </div>
+ *
+ *
+ * App component
+ *
+ * <Layout>
+ *  <main>
+ * </Layout>
+ */
+
+// Virtual DOM
 export const a = 1;
 export const foo = () => {
   console.log("foo function");
